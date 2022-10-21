601a89ab12dc42d6c643c34714e726108a7eb46f
unknown <deekshabisht16@gmail.com>
2022-10-21 19:07:27 +0530

    second commit

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from '../../Assets/Logo/logo.svg';
 import marker from '../../Assets/Images/marker.svg';
+import MillionStars from '../../Assets/Images/MillionStars.jpg';
+import starbucksReward from '../../Assets/Images/starbucksReward.webp';
+
 export default function HomePage(){
     return(
         <div>
@@ -44,6 +47,52 @@ export default function HomePage(){
             </button>
         </div>
     </nav>
+   
+    {/*box A*/}
+     
+    <div className='box'>
+    <div className="box-image">
+        <img src={MillionStars} alt=""/>
+    </div>
+    <div className='box-internal'>
+    <div className="box-text">
+            <h2 className="text-xl">Win a thousand Stars</h2>
+            <p className="text-md">
+            We’re giving away 1,000 Stars to 1,000 Starbucks® Rewards members in our Million Stars Giveaway and much more all week!*
+            </p>
+            <a href="#" className="btn btn-light-outline">Play to win</a>
+    </div>
+    </div>
+    </div>
+
+    {/*box B*/}
+
+    <div className='box'>
+    <div className='box-internal'>
+    <div className="box-text">
+            <h2 className="text-xl"> A new way to earn sips and trips </h2>
+            <p className="text-md">
+            Now you can link your Starbucks Rewards + Delta SkyMiles account to get:
+            </p>
+            <ul style={{listStyleType:'circle'}}>
+            <li>
+                150 Stars+500 miles when you link before 12/31
+            </li>
+            <li>
+                Double Stars on Delta travel days
+            </li>
+            <li>
+            1 mile per $1 spent at Starbucks(excludes tax and gratuities)**
+            </li>
+            </ul>
+          <a href="#" className="btn btn-light-outline">Link Account</a>
+    </div>
+    </div>
+    <div className="box-image">
+        <img src={starbucksReward} alt=""/>
+    </div>
+    </div>
+    
     </div>
     )
 }
